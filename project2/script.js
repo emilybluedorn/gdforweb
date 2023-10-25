29a52bcd669b7b5bbf175d3750bcf8ee5e94da2b
emilybluedorn <ebluedor@risd.edu>
2023-10-25 18:30:28 -0400

    update
    update

--- project2/script.js
+++ project2/script.js
@@ -26,7 +26,28 @@ setInterval(updateDateTime);
 
 // button attempt -- works but only for one color not both directions
 
-function changeClass() {
+function changeClass_2() {
   var element = document.querySelector("#blue");
-  element.classList.replace("cls-1", "cls-2");
+  element.removeAttribute("class");
+  element.classList.add('cls-2');
 }
+
+function changeClass_3() {
+  var element = document.querySelector("#blue");
+  element.removeAttribute("class");
+  element.classList.add('cls-3');
+}
+
+/* 0. define button*/
+var button = document.querySelector("#color").find('button');
+
+/* 
+  when button is clicked:
+    1. detect the data-number attribute of the clicked button 
+    2. set the value of the attribute as a variable 
+    3. print the variable as the part of class name like below 
+*/
+ 
+ element.classList.add('cls-' + number);
+
+
