8bffa5df362ae9b4fb2b23377b46d8a2c5d5a813
emilybluedorn <ebluedor@risd.edu>
2023-10-29 15:37:59 -0400

    update
    update

--- testcode/script1.js
+++ testcode/script1.js
@@ -1,12 +1,5 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-    this.className += " active";
-  });
-}
+console.log('isthisthingon');
+
+// document.getElementById("b").addEventListener('click', function () {  document.getElementById("p").innerHTML = "See";
+// document.getElementById("p").style.color="red";                                            
+// })
