c9db0c0ce5a9e50eaa865dfa130430c51a19b97e
emilybluedorn <ebluedor@risd.edu>
2023-10-30 13:12:12 -0400

    update test code
    update test code

--- testcode/script1.js
+++ testcode/script1.js
@@ -3,3 +3,19 @@ console.log('isthisthingon');
 // document.getElementById("b").addEventListener('click', function () {  document.getElementById("p").innerHTML = "See";
 // document.getElementById("p").style.color="red";                                            
 // })
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
