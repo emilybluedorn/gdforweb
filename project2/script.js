25c398bef705af7addcb144dea3957f62f09db20
emilybluedorn <ebluedor@risd.edu>
2023-12-08 23:45:42 -0500

    update
    update

--- project2/script.js
+++ project2/script.js
@@ -1,15 +1,3 @@
-// // Console Log
-
-// // create a new `Date` object
-// const now = new Date();
-
-// // get the current date and time as a string
-// const currentDateTime = now.toLocaleString();
-
-// console.log(currentDateTime); // output: "7/20/2021, 2:28:15 PM" (will vary depending on your time zone)
-
-
-// Actual script applied to page
 
 function updateDateTime() {
 
